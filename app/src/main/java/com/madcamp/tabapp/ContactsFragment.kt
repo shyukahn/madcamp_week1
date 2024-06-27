90ccbd959c970c4a310e62432c599eb7cc9cec61
Jinyshin <iwbm312@gmail.com>
2024-06-27 19:13:53 +0900

    Feat: create fragments and icon

---
+++ app/src/main/java/com/madcamp/tabapp/ContactsFragment.kt
@@ -0,0 +1,6 @@
+package com.madcamp.tabapp
+
+import androidx.fragment.app.Fragment
+
+class FirstFragment:Fragment(R.layout.fragment_first) {
+}
---
+++ app/src/main/java/com/madcamp/tabapp/PhotosFragment.kt
@@ -0,0 +1,6 @@
+package com.madcamp.tabapp
+
+import androidx.fragment.app.Fragment
+
+class SecondFragment:Fragment(R.layout.fragment_second) {
+}

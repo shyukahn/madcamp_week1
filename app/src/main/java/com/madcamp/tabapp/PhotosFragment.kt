970feda5954d8ef5ffcbd0d8da4c8034c4f4acbe
Sehyuk Ahn <shyukahn@gmail.com>
2024-06-28 13:56:34 +0900

    Feat: 사진 탭 RecyclerView 구현

--- app/src/main/java/com/madcamp/tabapp/PhotosFragment.kt
+++ app/src/main/java/com/madcamp/tabapp/PhotosFragment.kt
@@ -1,6 +1,29 @@
 package com.madcamp.tabapp
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.madcamp.tabapp.databinding.FragmentPhotosBinding
 
-class PhotosFragment:Fragment(R.layout.fragment_photos) {
+class PhotosFragment : Fragment(R.layout.fragment_photos) {
+    private lateinit var binding: FragmentPhotosBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPhotosBinding.inflate(layoutInflater)
+
+        val photoList = ArrayList<Int>((1..40).toList())
+        binding.rvPhotos.apply {
+            adapter = PhotosAdapter(requireContext(), photoList)
+            layoutManager = GridLayoutManager(context, 2)
+        }
+
+        return binding.root
+    }
 }

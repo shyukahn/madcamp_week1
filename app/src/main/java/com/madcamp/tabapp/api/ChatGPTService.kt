5a81aab334f02130098bb01f3ab64c3bcdb35232
Jinyshin <iwbm312@gmail.com>
2024-06-29 16:14:27 +0900

    feat: add api services

---
+++ app/src/main/java/com/madcamp/tabapp/api/ChatGPTService.kt
@@ -0,0 +1,4 @@
+package com.madcamp.tabapp.api
+
+interface ChatGPTService {
+}
---
+++ app/src/main/java/com/madcamp/tabapp/api/UnsplashService.kt
@@ -0,0 +1,8 @@
+package com.madcamp.tabapp.api
+
+/**
+ * Unsplash API에 연결하여 사진을 가져오기 위한 서비스
+ */
+
+interface UnsplashService {
+}

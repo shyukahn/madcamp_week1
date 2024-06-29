505fc8cf3476566447fce88ab5b7d44ce926a464
Sehyuk Ahn <shyukahn@gmail.com>
2024-06-29 17:45:45 +0900

    feat: create review dialog view

--- app/src/main/java/com/madcamp/tabapp/PhotosFragment.kt
+++ app/src/main/java/com/madcamp/tabapp/PhotosFragment.kt
@@ -4,21 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.madcamp.tabapp.databinding.FragmentPhotosBinding
 import com.madcamp.tabapp.data.PhotoModel
 import com.madcamp.tabapp.adapters.PhotosAdapter
+import com.madcamp.tabapp.dialogs.ReviewDialog
 
 class PhotosFragment : Fragment(R.layout.fragment_photos) {
     private lateinit var binding: FragmentPhotosBinding
+    private lateinit var photosAdapter: PhotosAdapter
     private val photoList = ArrayList<PhotoModel>()
+    private val pickMultipleMedia = registerForActivityResult(PickVisualMedia()) { uri ->
+        uri?.let {
+            ReviewDialog(requireContext(), photosAdapter, uri).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        repeat(32) {
-            photoList.add(PhotoModel(R.drawable.sungsimdang, "성심당"))
+        repeat(2) {
+            photoList.add(PhotoModel("성심당", id = R.drawable.sungsimdang))
         }
     }
 
@@ -28,10 +37,15 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentPhotosBinding.inflate(inflater, container, false)
+        photosAdapter = PhotosAdapter(this, photoList)
 
         binding.rvPhotos.apply {
-            adapter = PhotosAdapter(this@PhotosFragment, photoList)
-            layoutManager = GridLayoutManager(context, 2)
+            adapter = photosAdapter
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        }
+
+        binding.addButton.setOnClickListener {
+            pickMultipleMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))
         }
 
         return binding.root
--- app/src/main/java/com/madcamp/tabapp/data/PhotoModel.kt
+++ app/src/main/java/com/madcamp/tabapp/data/PhotoModel.kt
@@ -1,6 +1,12 @@
 package com.madcamp.tabapp.data
 
+import android.net.Uri
+
 data class PhotoModel(
-    val id: Int,
-    val text: String
-)
+    val text: String,
+    val id: Int? = null,
+    val uri: Uri? = null
+) {
+    val isId: Boolean
+        get() = id != null
+}

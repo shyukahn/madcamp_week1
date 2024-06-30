0d06efdcf028181bb7494f9147486d704fd51298
Sehyuk Ahn <shyukahn@gmail.com>
2024-07-01 01:16:19 +0900

    feat: create review_table in database

---
+++ app/src/main/java/com/madcamp/tabapp/data/ReviewDao.kt
@@ -0,0 +1,29 @@
+package com.madcamp.tabapp.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface ReviewDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(review: Review)
+
+    @Update
+    suspend fun update(review: Review)
+
+    @Delete
+    suspend fun delete(review: Review)
+
+    @Query("SELECT * from review_table WHERE id = :id")
+    fun getReviewById(id: Int): Review
+
+    @Query("SELECT * from review_table")
+    fun getAllReviews(): List<Review>
+
+    @Query("DELETE FROM review_table")
+    suspend fun deleteAll()
+}

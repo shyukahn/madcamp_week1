ecab74d64e97a67f8d88eaeb1c9954838a7e65f6
Jinyshin <iwbm312@gmail.com>
2024-07-02 01:03:13 +0900

    feat: change login logic to consider auto login

--- app/src/main/java/com/madcamp/tabapp/LoginActivity.kt
+++ app/src/main/java/com/madcamp/tabapp/LoginActivity.kt
@@ -3,16 +3,13 @@ package com.madcamp.tabapp
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.madcamp.tabapp.data.database.InitDb
 import com.madcamp.tabapp.data.User
 import com.madcamp.tabapp.data.UserDao
+import com.madcamp.tabapp.data.database.InitDb
 import com.madcamp.tabapp.databinding.ActivityLoginBinding
-
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -20,8 +17,6 @@ import kotlinx.coroutines.withContext
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var etLoginId: EditText
-    private lateinit var etPassword: EditText
     private lateinit var btnLogin: Button
     private lateinit var binding: ActivityLoginBinding
 
@@ -36,77 +31,67 @@ class LoginActivity : AppCompatActivity() {
         userDao = InitDb.appDatabase.userDao()
 
         // Initialize UI components
-        etLoginId = binding.etLoginId
-        etPassword = binding.etPassword
         btnLogin = binding.btnLogin
 
         // Set onClickListener for the login button
         btnLogin.setOnClickListener {
-            login()
+            val loginId = binding.etLoginId.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+            login(loginId, password)
         }
-
-        // Insert a test user for demonstration purposes
-        insertTestUser()
-
-        // print all users
-        loadAllUsers()
     }
 
-    private fun login() {
-
-        val loginId = etLoginId.text.toString().trim()
-        val password = etPassword.text.toString().trim()
+    private fun login(loginId: String, password: String) {
 
         if (TextUtils.isEmpty(loginId) || TextUtils.isEmpty(password)) {
             Toast.makeText(this, "로그인 정보를 입력하세요", Toast.LENGTH_SHORT).show()
             return
         }
 
-        // Execute the database query on a background thread using coroutines
         CoroutineScope(Dispatchers.IO).launch {
             val user = userDao.getUserByLoginId(loginId)
 
-            // Handle the result on the main thread
-            withContext(Dispatchers.Main) {
-                if (user != null && password == user.password) {
-                    // Successful login, navigate to the main activity
-                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                } else {
-                    // Invalid login credentials
-                    Toast.makeText(this@LoginActivity, "로그인 정보가 잘못되었습니다", Toast.LENGTH_SHORT).show()
+            if (user == null) {
+                // Check if the user_table has any data
+                val userCount = userDao.getUserCount()
+                if (userCount == 0) {
+                    insertTestUser(loginId, password)
+                    withContext(Dispatchers.Main) {
+                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                    return@launch // Early return to prevent executing the rest of the coroutine
+                }
+            } else {
+                // Handle the result on the main thread
+                withContext(Dispatchers.Main) {
+                    if (password == user.password) {
+                        // Successful login, navigate to the main activity
+                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // Invalid login credentials
+                        Toast.makeText(this@LoginActivity, "로그인 정보가 잘못되었습니다", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
         }
+
     }
 
-    private fun insertTestUser() {
+    private fun insertTestUser(loginId: String, password: String) {
         CoroutineScope(Dispatchers.IO).launch {
-            // Check if the test user 'admin' already exists in the db
-            if (userDao.getUserByLoginId("admin") == null) {
-                // Insert the test user
-                val testUser = User(
-                    loginId = "admin",
-                    password = "admin",  // Note: In a real application, passwords should be hashed
-                    fullName = "Admin User",
-                    contact = "admin@example.com"
-                )
-
-                userDao.insert(testUser)
-            }
-        }
-    }
-
-    private fun loadAllUsers() {
-        CoroutineScope(Dispatchers.IO).launch {
-            val userDao = InitDb.appDatabase.userDao()
-            val userList = userDao.getAllUsers()
-
-            // 로그를 통해 사용자 데이터를 콘솔에 출력
-            userList.forEach { user ->
-                Log.d("MainActivity", "유저 정보 User: ${user.loginId}, ${user.fullName}, ${user.contact}")
-            }
+            // Insert the test user
+            val testUser = User(
+                loginId = loginId,
+                password = password,  // Note: In a real application, passwords should be hashed
+                // TODO: change fullName
+                nickname = "Admin User",
+                contact = "admin@example.com"
+            )
+            userDao.insert(testUser)
         }
     }
 }

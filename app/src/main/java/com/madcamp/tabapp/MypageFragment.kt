4900a916f7934062fbc4d2a2d2fe20362f7b9e50
Jinyshin <iwbm312@gmail.com>
2024-07-01 23:39:34 +0900

    feat: change my page username

--- app/src/main/java/com/madcamp/tabapp/MypageFragment.kt
+++ app/src/main/java/com/madcamp/tabapp/MypageFragment.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.madcamp.tabapp.data.database.InitDb
 import com.madcamp.tabapp.databinding.FragmentMypageBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MypageFragment : Fragment(R.layout.fragment_mypage) {
 
@@ -15,6 +19,14 @@ class MypageFragment : Fragment(R.layout.fragment_mypage) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentMypageBinding.bind(view)
 
+        CoroutineScope(Dispatchers.IO).launch {
+            val userDao = InitDb.appDatabase.userDao()
+            // TODO: test user "admin" -> refactor using variable
+            val user = userDao.getUserByLoginId("admin")
+            if (user != null) {
+                binding.nickname.text = user.fullName
+            }
+        }
         binding.bookmarkedBakeries.setOnClickListener {
             val intent = Intent(context, BookmarkedBakeriesActivity::class.java)
             startActivity(intent)

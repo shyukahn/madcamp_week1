6e67e963d6fa9d0eb37be63510bb1d0bd2b66d5f
Sehyuk Ahn <shyukahn@gmail.com>
2024-07-03 16:55:03 +0900

    feat: added default profile images

--- app/src/main/java/com/madcamp/tabapp/PhotosFragment.kt
+++ app/src/main/java/com/madcamp/tabapp/PhotosFragment.kt
@@ -89,7 +89,7 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
                 reviewText = "성심당 본점은 항상 신선한 빵과 친절한 서비스로 유명하죠. 특히 튀김소보로가 최고예요.",
                 imageUri = "android.resource://com.madcamp.tabapp/" + R.drawable.review1,
                 writer = "민수잇",
-                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image,
+                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image1,
                 isAdminUser = false
             ),
             Review(
@@ -97,7 +97,7 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
                 reviewText = "대전역에서 기차 타기 전에 들렀는데, 빵이 너무 맛있어서 놀랐어요.",
                 imageUri = "android.resource://com.madcamp.tabapp/" + R.drawable.review2,
                 writer = "여행자",
-                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image,
+                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image2,
                 isAdminUser = false
             ),
             Review(
@@ -105,7 +105,7 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
                 reviewText = "당신을 위한 빵집은 아늑한 분위기에서 맛있는 빵을 즐길 수 있어서 좋아요.",
                 imageUri = "android.resource://com.madcamp.tabapp/" + R.drawable.review3,
                 writer = "달콤한하루",
-                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image,
+                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image3,
                 isAdminUser = false
             ),
             Review(
@@ -113,7 +113,7 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
                 reviewText = "캘리포니아 베이커리 카페는 미국 느낌의 빵이 많아서 좋아요. 맛도 훌륭합니다.",
                 imageUri = "android.resource://com.madcamp.tabapp/" + R.drawable.review4,
                 writer = "캘리러버",
-                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image,
+                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image4,
                 isAdminUser = false
             ),
             Review(
@@ -121,7 +121,7 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
                 reviewText = "인유단 베이커리는 전통의 맛을 그대로 느낄 수 있는 빵이 많아서 좋아요.",
                 imageUri = "android.resource://com.madcamp.tabapp/" + R.drawable.review5,
                 writer = "전통맛",
-                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image,
+                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image5,
                 isAdminUser = false
             ),
             Review(
@@ -129,7 +129,7 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
                 reviewText = "따뜻한 분위기에서 빵을 즐길 수 있어서 자주 방문해요.",
                 imageUri = "android.resource://com.madcamp.tabapp/" + R.drawable.review6,
                 writer = "따뜻함",
-                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image,
+                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image6,
                 isAdminUser = false
             ),
             Review(
@@ -137,7 +137,7 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
                 reviewText = "빵 품질이 너무 좋아서 항상 만족스럽습니다. 강추!",
                 imageUri = "android.resource://com.madcamp.tabapp/" + R.drawable.review7,
                 writer = "품질우선",
-                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image,
+                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image7,
                 isAdminUser = false
             ),
             Review(
@@ -145,7 +145,7 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
                 reviewText = "슬로우 브레드는 건강한 재료로 만든 빵이라 안심하고 먹을 수 있어요.",
                 imageUri = "android.resource://com.madcamp.tabapp/" + R.drawable.review8,
                 writer = "헬시러버",
-                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image,
+                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image8,
                 isAdminUser = false
             ),
             Review(
@@ -153,7 +153,7 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
                 reviewText = "백화점 안에 있어서 그런지 분위기가 고급스럽고 빵도 최고예요.",
                 imageUri = "android.resource://com.madcamp.tabapp/" + R.drawable.review9,
                 writer = "고급진삶",
-                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image,
+                profileUri = "android.resource://com.madcamp.tabapp/" + R.drawable.profile_image9,
                 isAdminUser = false
             )
         )
